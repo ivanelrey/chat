c7946529413febcaef9e7c64e3487d9f85f16939
Smartup <smartup@iMac-Alexandros.local>
2017-02-23 16:56:16 +0200

    logged in def added?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :logged_in?
+
+  def current_user
+  	  @current_user ||= User.find(session[:user_id])	if session[:user_id] #return @current_user if exists otherwise find from session and the return it 
+  end
+
+  def logged_in?
+  	!!current_user#returns true if current_user exists otherwise returns false
+  end
+
+  def require_user
+  	if logged_in?
+  		flash[:danger] = "You must be logged in to perform that action"
+  		redirect_to root_path
+  	end
+  end
 end
