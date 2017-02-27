f670877cb6ed5ab5a0927309babf9fb139191a80
ivane gomarteli <ivanejg@yahoo.com>
2017-02-27 19:46:04 +0200

    asd

--- db/migrate/20170224154125_change_foreign_keys_type_in_friendships.rb
+++ db/migrate/20170224154125_change_foreign_keys_type_in_friendships.rb
@@ -1,6 +1,6 @@
 class ChangeForeignKeysTypeInFriendships < ActiveRecord::Migration[5.0]
   def change
-  	change_column :friendships, :user_id, 'integer USING CAST(column_name AS integer)'
-  	change_column :friendships, :friend_id, 'integer USING CAST(column_name AS integer)'
+  	change_column :friendships, :user_id, 'integer USING CAST(user_id AS integer)'
+  	change_column :friendships, :friend_id, 'integer USING CAST(friend_id AS integer)'
   end
 end
