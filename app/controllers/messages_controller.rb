08f69d7cf8474ec975b9505b046d817c4652b0f5
Smartup <smartup@iMac-Alexandros.local>
2017-03-09 19:17:53 +0200

    change few things

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController 
 
 	def show
-		@conversation = current_user.messages.where(receiver_id: params[:id]) + current_user.inverse_messages.where(user_id: params[:id])
-		@conversation = @conversation.sort_by{|e| e[:created_at]}
+		@messages = current_user.messages.where(receiver_id: params[:id]) + current_user.inverse_messages.where(user_id: params[:id])
+		@messages = @messages.sort_by{|e| e[:created_at]}
 		@receiver_id = params[:id]
 	end
 
@@ -10,6 +10,9 @@ class MessagesController < ApplicationController
 	def create
 		#render plain: params[:message].inspect
 		Message.create(user_id: current_user.id, receiver_id: params[:message][:receiver_id], text: params[:message][:text])
+		@conversation = current_user.messages.where(receiver_id: params[:message][:receiver_id]) + current_user.inverse_messages.where(user_id: params[:message][:receiver_id])
+		@conversation = @conversation.sort_by{|e| e[:created_at]}
+		@receiver_id = params[:id]
 		respond_to do |format|
     		format.js { render :action => "refresh_conversation" }
  		end
